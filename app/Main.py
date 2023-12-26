ab8634ec596cc54b489a4597229143e56bc32c7a
David <davidcharles115@gmail.com>
2023-12-27 00:33:05 +0530

    26/12/23

--- app/Main.py
+++ app/Main.py
@@ -2,13 +2,23 @@
 This is my main file where the program start's
 """
 from features.NewAccount import account_opening
-# Welcome message
-print("Welcome to the ABZ Bank Please select an option")
-print("1. Open Account\n"
-      "2. Delete Account\n"
-      "3. View Account\n"
-      "4. Bank Operation\n")
-choosen_option = int(input("Enter your Option Here - "))
-if choosen_option == 1:
-    account_opening()
+import features
 
+# Welcome message
+object = features.NewAccount.Features()
+while True:
+    print("Welcome to the ABZ Bank Please select an option")
+    print("1. Open Account\n"
+          "2. Delete Account\n"
+          "3. View Account\n"
+          "4. Bank Operation\n")
+    chosen_option = int(input("Enter your Option Here - "))
+    if chosen_option == 1:
+        account_opening()
+    elif chosen_option == 2:
+        delete = int(input("Enter your account number - \n"))
+        object.delete(delete)
+    elif chosen_option == 3:
+        print("work in progress")
+    elif chosen_option == 4:
+        print("work in progress")
--- features/NewAccount.py
+++ features/NewAccount.py
@@ -1,13 +1,54 @@
-def account_opening():
-    account_holder_name = str(input("Enter Your good name\n"))
-    mobile_number = int(input("Enter your mobile number\n"))
-    pan_card = int(input("Enter Pan card Number\n"))
-    email_id = list(input("Your email id\n"))
+import random
 
-    print(f"Check the details to confirm \n"
-          f"Name : {account_holder_name}\n"
-          f"Mobile : {mobile_number}\n"
-          f"Pan card : {pan_card}\n"
-          f"Email id : {email_id}\n")
+
+class Features:
+    account_detail = []
+
+
+    def append(self,item):
+        self.account_detail.append(item)
+
+    def delete(self,num):
+        if self.account_detail.__contains__(num):
+            self.account_detail.remove(num)
+            print("Account deleted successfully")
+        else:
+            print("No account found please check your account number")
+
+
+#making class object
+feature = Features()
+
+"""
+Function for account opening process
+"""
+def account_opening():
+    while True:
+        account_holder_name = str(input("Enter Your good name\n"))
+        mobile_number = int(input("Enter your mobile number\n"))
+        pan_card = int(input("Enter Pan card Number\n"))
+        email_id = list(input("Your email id\n"))
+
+        print(f"Check the details to confirm \n"
+              f"Name : {account_holder_name}\n"
+              f"Mobile : {mobile_number}\n"
+              f"Pan card : {pan_card}\n"
+              f"Email id : {email_id}\n")
+        confirm = input("Enter your response Y for yes N for No - ")
+        rs = confirm.lower()
+        if rs == "y":
+            account_number = random.randint(0, 100000000000) * 10
+            feature.append(account_number)
+            print(f"Note your account number - {account_number}")
+
+            break
+        else:
+            print("Re-enter your detail's")
+            continue
+
+def delete_account(account_num):
+    feature.delete(account_num)
+
+
 
 
