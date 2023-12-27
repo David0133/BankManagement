dd65177fbfa2e861fcaeb75d61c3179b951fa651
David <davidcharles115@gmail.com>
2023-12-27 23:16:45 +0530

    27/12/2023

--- app/Main.py
+++ app/Main.py
@@ -5,20 +5,27 @@ from features.NewAccount import account_opening
 import features
 
 # Welcome message
-object = features.NewAccount.Features()
 while True:
-    print("Welcome to the ABZ Bank Please select an option")
-    print("1. Open Account\n"
-          "2. Delete Account\n"
-          "3. View Account\n"
-          "4. Bank Operation\n")
-    chosen_option = int(input("Enter your Option Here - "))
-    if chosen_option == 1:
-        account_opening()
-    elif chosen_option == 2:
-        delete = int(input("Enter your account number - \n"))
-        object.delete(delete)
-    elif chosen_option == 3:
-        print("work in progress")
-    elif chosen_option == 4:
-        print("work in progress")
+    try:
+        print("Welcome to the ABZ Bank Please select an option")
+        print("1. Open Account\n"
+              "2. Delete Account\n"
+              "3. View Account\n"
+              "4. Bank Operation\n"
+              "5. Exit\n")
+        chosen_option = int(input("Enter your Option Here - "))
+        if chosen_option == 1:
+            account_opening()
+        elif chosen_option == 2:
+            delete = int(input("Enter your account number - \n"))
+            features.NewAccount.feature.delete(delete)
+        elif chosen_option == 3:
+            number = int(input("Enter your account number - \n"))
+            features.NewAccount.account_detail(number)
+        elif chosen_option == 4:
+            print("work in progress")
+        elif chosen_option == 5:
+            print("Thanks for using ABZ bank service")
+            break
+    except ValueError:
+        print("Invalid option please enter a valid response")
--- features/NewAccount.py
+++ features/NewAccount.py
@@ -1,27 +1,42 @@
 import random
 
 
+# Features class for some variable declaration and functions
 class Features:
     account_detail = []
 
-
-    def append(self,item):
+    def append(self, item):
         self.account_detail.append(item)
 
-    def delete(self,num):
-        if self.account_detail.__contains__(num):
-            self.account_detail.remove(num)
-            print("Account deleted successfully")
-        else:
-            print("No account found please check your account number")
+    def delete(self, num):
+        for i, o in enumerate(self.account_detail):
+            if o.num == num:
+                del self.account_detail[i]
+                print("Account deleted successfully")
+                break
+            else:
+                print("No account found please check your account number")
 
 
-#making class object
+# making class object
 feature = Features()
+
+
+# Account class for account detail initialization
+class Account:
+    def __init__(self, account_num, name, mobile, pan, email):
+        self.num = account_num
+        self.name = name
+        self.mobile = mobile
+        self.pan = pan
+        self.email = email
+
 
 """
 Function for account opening process
 """
+
+
 def account_opening():
     while True:
         account_holder_name = str(input("Enter Your good name\n"))
@@ -38,17 +53,25 @@ def account_opening():
         rs = confirm.lower()
         if rs == "y":
             account_number = random.randint(0, 100000000000) * 10
-            feature.append(account_number)
+            feature.account_detail.append(
+                Account(account_number, account_holder_name, mobile_number, pan_card, email_id))
             print(f"Note your account number - {account_number}")
-
             break
         else:
             print("Re-enter your detail's")
             continue
 
-def delete_account(account_num):
-    feature.delete(account_num)
 
-
-
-
+# Function for account details
+def account_detail(account_num):
+    for i in feature.account_detail:
+        if i.num == account_num:
+            print(f"******** Your Account Details **********\n"
+                  f"Your name - {i.name}\n    "
+                  f"Your Mobile - {i.mobile}\n"
+                  f"Your Pan Card - {i.pan}\n "
+                  f"Your Email - {i.email}\n  ")
+            return
+        else:
+            continue
+    print(f"No such account found with the number {account_num}")
